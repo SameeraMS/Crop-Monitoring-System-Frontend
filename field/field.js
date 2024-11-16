b694b7899a7940f8f0521c48ceb5cd5c365bd902
Sameera Madushan Silva <madushansameera499@gmail.com>
2024-11-16 23:59:12 +0530

    add field image properly

--- field/field.js
+++ field/field.js
@@ -6,6 +6,19 @@ function initializeField() {
     loadLogIdsOnField();
 }
 
+function encodeFieldImage(imageFile) {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onload = function (event) {
+            resolve(event.target.result); // Ensure 'event' is correctly passed
+        };
+        reader.onerror = function (error) {
+            reject(error); // Handle errors properly
+        };
+        reader.readAsDataURL(imageFile);
+    });
+}
+
 function loadFieldTable() {
     $.ajax({
         url: "http://localhost:8082/cms/api/v1/fields",
@@ -24,6 +37,38 @@ function loadFieldTable() {
             console.error(res);
         }
     });
+}
+
+function saveFieldImage(fieldId, image1, image2) {
+    if (image1 != null || image2 != null) {
+        const formData = new FormData();
+
+        formData.append('fieldId', fieldId);
+        formData.append('image1', image1);
+        formData.append('image2', image2);
+
+        $.ajax({
+            url: "http://localhost:8082/cms/api/v1/fields",
+            type: "POST",
+            data: formData,
+            contentType: false,
+            processData: false,
+            headers: {
+                "Authorization": "Bearer " + localStorage.getItem('token')
+            },
+            success: (res) => {
+                console.log(res);
+                initializeField()
+            },
+            error: (res) => {
+                console.error(res);
+            }
+        });
+    } else {
+        initializeField()
+    }
+
+
 }
 
 function loadLogIdsOnField() {
@@ -56,21 +101,39 @@ document.getElementById('field-form').addEventListener('submit', function(e) {
     const fieldName = document.getElementById('fieldName').value;
     let fieldLocation = document.getElementById('fieldLocation').value;
     const fieldSize = document.getElementById('fieldSize').value;
-    let image1 = document.getElementById('image1').value;
-    let image2 = document.getElementById('image2').value;
+    let imageFile1 = document.getElementById('image1').files[0];
+    let imageFile2 = document.getElementById('image2').files[0];
     let logId = document.getElementById('logIdOnField').value;
-
-    if (image1 === '') {
-        image1 = null;
-    }
-
-    if (image2 === '') {
-        image2 = null;
-    }
 
     if (logId === 'select log') {
         logId = null;
     }
+
+    let image1 = null;
+    let image2 = null;
+
+    (async function () {
+        if (imageFile1) {
+            try {
+                image1 = await encodeFieldImage(imageFile1);
+            } catch (error) {
+                console.error("Error encoding image:", error);
+            }
+        } else {
+            console.log("No file selected.");
+        }
+
+        if (imageFile2) {
+            try {
+                image2 = await encodeFieldImage(imageFile2);
+            } catch (error) {
+                console.error("Error encoding image:", error);
+            }
+        } else {
+            console.log("No file selected.");
+        }
+    })();
+
 
     fieldLocation = extractCoordinates(fieldLocation);
 
@@ -78,8 +141,6 @@ document.getElementById('field-form').addEventListener('submit', function(e) {
         fieldName,
         fieldLocation,
         fieldSize,
-        image1,
-        image2,
         logId
     };
 
@@ -93,6 +154,8 @@ document.getElementById('field-form').addEventListener('submit', function(e) {
         contentType: "application/json",
         success: (res) => {
             console.log(res);
+            var fieldId = res.fieldId;
+            saveFieldImage(fieldId, image1, image2);
             initializeField();
             clearFieldForm();
         },
@@ -127,16 +190,37 @@ function addFieldToTable(field) {
     const tableBody = document.querySelector('#field-list tbody');
     const row = document.createElement('tr');
 
+    const img1 = document.createElement('img');
+    const img2 = document.createElement('img');
+
+    if (field.image1 != null) {
+        img1.src = `${field.image1}`;
+    }
+
+    if (field.image2 != null) {
+        img2.src = `${field.image2}`;
+    }
+
+    img1.alt = 'Field Image';
+    img1.style.width = '50px'; // Adjust size as needed
+    img1.style.height = '50px'; // Adjust size as needed
+
+    img2.alt = 'Field Image';
+    img2.style.width = '50px'; // Adjust size as needed
+    img2.style.height = '50px'; // Adjust size as needed
+
     row.innerHTML = `
         <td>${field.fieldId}</td>
         <td>${field.fieldName}</td>
         <td>${field.fieldSize}</td>
-        <td><img src="" alt="Image 1" style="width: 50px;" /></td>
-        <td><img src="" alt="Image 2" style="width: 50px;" /></td>
+        <td></td>
+        <td></td>
         <td><button value="${field.fieldId}" class="edit-btn" onclick="editField(this)">Edit</button></td>
         <td><button value="${field.fieldId}" class="delete-btn" onclick="deleteField(this)">Delete</button></td>
     `;
 
+    row.cells[3].appendChild(img1);
+    row.cells[4].appendChild(img2);
     tableBody.appendChild(row);
 }
 
@@ -182,17 +266,6 @@ function editField(button) {
             document.getElementById('fieldName').value = res.fieldName;
             document.getElementById('fieldLocation').value = res.fieldLocation.x + ', ' + res.fieldLocation.y;
             document.getElementById('fieldSize').value = res.fieldSize;
-            if (res.image1 === null) {
-                document.getElementById('image1').value = '';
-            } else {
-                document.getElementById('image1').value = res.image1;
-            }
-
-            if (res.image2 === null) {
-                document.getElementById('image2').value = '';
-            } else {
-                document.getElementById('image2').value = res.image2;
-            }
 
             if (res.logId === null) {
                 $('#logIdOnField').val('Select Log');
@@ -206,3 +279,64 @@ function editField(button) {
     });
 
 }
+
+$('#updateFieldBtn').on('click', () => {
+    const fieldName = document.getElementById('fieldName').value;
+    let fieldLocation = document.getElementById('fieldLocation').value;
+    const fieldSize = document.getElementById('fieldSize').value;
+    const logId = document.getElementById('logIdOnField').value;
+    let imageFile1 = document.getElementById('image1').files[0];
+    let imageFile2 = document.getElementById('image2').files[0];
+
+    let image1 = null;
+    let image2 = null;
+
+    (async function () {
+        if (imageFile1) {
+            try {
+                image1 = await encodeFieldImage(imageFile1);
+            } catch (error) {
+                console.error("Error encoding image:", error);
+            }
+        } else {
+            console.log("No file selected.");
+        }
+
+        if (imageFile2) {
+            try {
+                image2 = await encodeFieldImage(imageFile2);
+            } catch (error) {
+                console.error("Error encoding image:", error);
+            }
+        } else {
+            console.log("No file selected.");
+        }
+    })();
+
+    fieldLocation = extractCoordinates(fieldLocation);
+
+    const field = {
+        fieldName,
+        fieldLocation,
+        fieldSize,
+        logId
+    };
+
+    $.ajax({
+        url: "http://localhost:8082/cms/api/v1/fields/" + updateFieldId,
+        type: "PUT",
+        headers: {
+            "Authorization": "Bearer " + localStorage.getItem('token')
+        },
+        data: JSON.stringify(field),
+        contentType: "application/json",
+        success: (res) => {
+            console.log(res);
+            saveFieldImage(updateFieldId, image1, image2);
+            initializeField();
+        },
+        error: (res) => {
+            console.error(res);
+        }
+    });
+})
